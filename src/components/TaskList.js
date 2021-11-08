e5b8eccacb995e9baae85a2f3a13fbcc8fcd3c41
Kevin Ebaugh <kevinebaugh@gmail.com>
2021-11-07 20:50:53 -0500

    Done.

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasksData}) {
+  const [tasks, setTasks] = useState(tasksData)
+
+  function handleDelete(event) {
+    const textToDelete = event.target.parentNode.children[1].innerText
+    setTasks(tasks.filter( task => task.text !== textToDelete))
+  }
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map( (task, index) => {
+        return <Task category={task["category"]} text={task["text"]} key={index} handleDelete={handleDelete}/>
+      })}
     </div>
   );
 }
